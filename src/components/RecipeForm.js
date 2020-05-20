9c16a35f4163aec23bcdb5580e8efe5c5184fb1e
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-05-20 19:35:43 -0400

    added Recipe.js and RecipeForm.js

---
+++ src/components/RecipeForm.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+function RecipeForm() {
+    //title input = string - text
+    //ingredients input = arr
+    //steps input = arr or string
+    //tags input = checkbox?
+    //source input = string - text
+}
+
+export default RecipeForm;
