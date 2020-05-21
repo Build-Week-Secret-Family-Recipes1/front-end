75585a9067040c9b5018e9b4421dc63d73b06e52
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-05-20 20:30:11 -0400

    added Home.js and RecipeList.js

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <h1>Secret Family Recipes</h1>
+        {/*
+            will show a list of recipes
+            will have button to add a new recipe
+        */}
+    )
+}
+
+export default Home;
---
+++ src/components/RecipeList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function RecipeList() {
+    return (
+        <div className="recipe-list-wrapper">
+            {/* 
+                will map over data and add a recipe card for each recipe
+                each recipe card will show the title, source, and tags
+                will link to full recipe
+            */}
+        </div>
+    )
+}
+
+export default RecipeList;
