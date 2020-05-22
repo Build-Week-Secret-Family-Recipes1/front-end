52a2cd2d0e5eaa9a6b0055f031abf32ea0290c51
Matthew Taylor <majikpig@gmail.com>
2020-05-21 20:04:36 -0400

    added PUT and DELETE actions and reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,6 +7,15 @@ export const FETCHING_RECIPE_FAILURE = "FETCHING_RECIPE_FAILURE";
 export const FETCHING_LIST_START = "FETCHING_LIST_START";
 export const FETCHING_LIST_SUCCESS = "FETCHING_LIST_SUCCESS";
 export const FETCHING_LIST_FAILURE = "FETCHING_LIST_FAILURE";
+export const POSTING_RECIPE_START = "POSTING_RECIPE_START";
+export const POSTING_RECIPE_SUCCESS = "POSTING_RECIPE_SUCCESS";
+export const POSTING_RECIPE_FAILURE = "POSTING_RECIPE_FAILURE";
+export const UPDATING_RECIPE_START = "UPDATING_RECIPE_START";
+export const UPDATING_RECIPE_SUCCESS = "UPDATING_RECIPE_SUCCESS";
+export const UPDATING_RECIPE_FAILURE = "UPDATING_RECIPE_FAILURE";
+export const DELETING_RECIPE_START = "DELETING_RECIPE_START";
+export const DELETING_RECIPE_SUCCESS = "DELETING_RECIPE_SUCCESS";
+export const DELETING_RECIPE_FAILURE = "DELETING_RECIPE_FAILURE";
 
 export const getRecipe = (recipeId) => async dispatch => {
   dispatch({ type: FETCHING_RECIPE_START, payload: recipeId });
@@ -48,3 +57,66 @@ export const getList = () => async dispatch => {
       });
     });
 };
+
+export const postRecipe = (recipe) => async dispatch => {
+  dispatch({ type: POSTING_RECIPE_START, payload: recipe });
+
+  axiosWithAuth
+    .post(`/recipes`, recipe)
+    .then(res => {
+      console.log(res);
+
+      dispatch({ type: POSTING_RECIPE_SUCCESS, payload: res.data });
+    })
+    .catch(err => {
+      console.log(err);
+      console.log(err.json);
+
+      dispatch({
+        type: POSTING_RECIPE_FAILURE,
+        payload: `${err.statusText} with response code ${err.status}, ${err}`
+      });
+    });
+};
+
+export const updateRecipe = (recipe) => async dispatch => {
+  dispatch({ type: UPDATING_RECIPE_START, payload: recipe });
+
+  axios
+    .put(`recipes/${recipe.id}`, recipe)
+    .then(res => {
+      console.log(res);
+
+      dispatch({ type: UPDATING_RECIPE_SUCCESS, payload: res.data });
+    })
+    .catch(err => {
+      console.log(err);
+      console.log(err.json);
+
+      dispatch({
+        type: UPDATING_RECIPE_FAILURE,
+        payload: `${err.statusText} with response code ${err.status}, ${err}`
+      });
+    });
+}
+
+export const deleteRecipe = (recipe) => async dispatch => {
+  dispatch({ type: DELETING_RECIPE_START, payload: recipe });
+
+  axios
+    .delete(`recipes/${recipe.id}`)
+    .then(res => {
+      console.log(res);
+
+      dispatch({ type: DELETING_RECIPE_SUCCESS, payload: res.data });
+    })
+    .catch(err => {
+      console.log(err);
+      console.log(err.json);
+
+      dispatch({
+        type: DELETING_RECIPE_FAILURE,
+        payload: `${err.statusText} with response code ${err.status}, ${err}`
+      });
+    });
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,19 @@
 import {
+  POSTING_RECIPE_START,
+  POSTING_RECIPE_SUCCESS,
+  POSTING_RECIPE_FAILURE,
   FETCHING_RECIPE_START,
   FETCHING_RECIPE_SUCCESS,
   FETCHING_RECIPE_FAILURE,
   FETCHING_LIST_START,
   FETCHING_LIST_SUCCESS,
   FETCHING_LIST_FAILURE,
+  UPDATING_RECIPE_START,
+  UPDATING_RECIPE_SUCCESS,
+  UPDATING_RECIPE_FAILURE,
+  DELETING_RECIPE_START,
+  DELETING_RECIPE_SUCCESS,
+  DELETING_RECIPE_FAILURE,
 } from "../actions";
 
 const initialState = {
@@ -17,6 +26,25 @@ const initialState = {
 
 export const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case POSTING_RECIPE_START:
+      return {
+        ...state,
+        recipe: action.payload,
+        isPosting: true
+      };
+    case POSTING_RECIPE_SUCCESS:
+      return {
+        ...state,
+        isPosting: false,
+        error: "",
+        recipe: action.payload
+      };
+    case POSTING_RECIPE_FAILURE:
+      return {
+        ...state,
+        isPosting: false,
+        error: action.payload
+      };
     case FETCHING_RECIPE_START:
       return {
         ...state,
@@ -52,6 +80,44 @@ export const reducer = (state = initialState, action) => {
         isFetching: false,
         error: action.payload
       };
+    case UPDATING_RECIPE_START:
+      return {
+        ...state,
+        isPosting: true,
+        recipe: action.payload
+      };
+    case UPDATING_RECIPE_SUCCESS:
+      return {
+        ...state,
+        isPosting: false,
+        error: "",
+        list: action.payload
+      };
+    case UPDATING_RECIPE_FAILURE:
+      return {
+        ...state,
+        isPosting: false,
+        error: action.payload
+        };
+    case DELETING_RECIPE_START:
+      return {
+        ...state,
+        recipe: action.payload,
+        isPosting: true
+      };
+    case DELETING_RECIPE_SUCCESS:
+      return {
+        ...state,
+        isPosting: false,
+        error: "",
+        list: action.payload
+      };
+    case DELETING_RECIPE_FAILURE:
+      return {
+        ...state,
+        isPosting: false,
+        error: action.payload
+        };
     default:
       return state;
   }
