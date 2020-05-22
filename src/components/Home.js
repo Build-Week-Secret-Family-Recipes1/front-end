7aae80d69ee650e023c788dbdc625a9cdb931044
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-05-21 21:56:10 -0400

    set up Home.js with recipes state, rendered RecipeForm and passed it setRecipes as props, rendered RecipeList and passed it recipes as props

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import RecipeForm from './RecipeForm';
+import RecipeList from './RecipeList';
 
 function Home() {
+    const [recipes, setRecipes] = useState([]);
+
     return (
         <div>
             <h1>Secret Family Recipes</h1>
             <div>
+                <RecipeForm setRecipes={setRecipes}/>
+                <RecipeList recipes={recipes}/>
                 {/*
                 will show a list of recipes
                 will have button to add a new recipe (will link to RecipeForm)
