5816b7656ea1b0ffd53ee6ed859bfe6cdad8f7ad
Matthew Taylor <majikpig@gmail.com>
2020-05-24 22:00:26 -0400

    register, login, logout working

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,9 +1,14 @@
 import React, {useEffect} from 'react';
+import {axiosWithAuth} from '../utils/axiosWithAuth';
 
 export default function Logout({history, func}) {
 
   useEffect(()=>{
-    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    axiosWithAuth()
+      .get('logout')
+      .then(res=>console.log(res))
+      .catch(err => console.log(err.message))
     func();
     history.push("/");
   },[history, func])
--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -2,12 +2,13 @@ import axios from "axios";
 
 
 export const axiosWithAuth = () => {
-  const token = localStorage.getItem("token");
+  const user = localStorage.getItem("user");
 
   return axios.create({
     baseURL: "https://secretrecipesbuild.herokuapp.com/",
     headers: {
-      Authorization: token
+      withCredentials: true,
+      user: user
     }
   });
 };
