18a01baead8b5c5c60d0d4e5b69f0f84827bed59
Adam Osburn <wizardofosburn@gmail.com>
2020-05-24 20:40:37 -0400

    added link to recipe form

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 
 function Home() {
     return (
         <div>
-            <h1>Secret Family Recipes</h1>
+            <div>
+               <h1>Secret Family Recipes</h1>
+               <Link to='/new' value='Add Recipe' />
+            </div>
+            
             <div>
                 {
                   recipes.map( recipe => (
