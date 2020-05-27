f69eacbe8db447a111c4c681a094bc2562a2ee8e
Adam Osburn <wizardofosburn@gmail.com>
2020-05-26 23:49:51 -0400

    Broke down into components, return Inner component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,44 +1,14 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
+import {Link, useParams} from 'react-router-dom'
 import {testList as recipes} from '../tests/TestData'
+import Inner from './Inner'
 
 function Home(props) {
+    const {params} = useParams();
 console.log(recipes)
     return (
         <div>
-            <div className='header-wrapper'>
-                <h1>Secret Family Recipes</h1>
-                <Link to='/new'>New Recipe</Link>
-            </div>
-            <div style={{
-                border: '2px solid gray',
-                backgroundColor: 'blue',
-                display: 'flex',
-                flexDirection: 'column'
-            }}>
-                {
-                  recipes.map( recipe => (
-                      <div key={recipe.id} style={{
-                                  border: '2px solid black',
-                                  display: 'flex',
-                                  flexDirection: 'column'
-                              }}>
-                          <h3 className="recipe" >{recipe.title}</h3>
-                          {recipes.source && <p className='sources'>{recipe.source}</p>}
-                          <p className="recipe-intro">Ingredients</p>
-                          {recipe.ingredients.map(ingredient => (
-                              <div className='ingredient-container'>
-                                 <p className='ingredient'>{ingredient}</p>
-                              </div>
-                          ))}
-        {/* newStep: '',
-        steps: [],
-        newTag: '',
-        tags: [] */}
-                      </div>
-                  ))
-                }
-            </div>
+           <Inner recipes={recipes}/>
         </div>
     )
 }
