3530b917aa95f55a1ca5f3622d6e720c4b340a18
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-05-28 16:24:31 -0400

    styled the search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,23 +1,84 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import { testList } from '../tests/TestData';
 
+const SearchForm = styled.form`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+    margin: auto;
+    width: 90%;
+`
+
+const Label = styled.label`
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    padding: 0;
+    width: 25rem;
+`
+
+const Input = styled.input`
+    margin: 0;
+    width: 10rem;
+    padding: 5px;
+    font-size: 1rem;
+    border: 2px solid #6bab90;
+    border-radius: 3px;
+    color: #5e4c5a;
+`
+
+const Select = styled.select`
+    font-size: 1rem;
+    color: #5e4c5a;
+    border: 2px solid #55917F;
+    border-radius: 3px;
+    padding: 5px;
+    width: 9rem;
+`
+
+const H4 = styled.h4`
+    margin: 0;
+    font-size: 1.6rem;
+    color: #5e4c5a;
+`
+
+const H5 = styled.h5`
+    margin: 0;
+    font-size: 1.1rem;
+    color: #5e4c5a;
+`
+
+const Button = styled.button`
+    display: inline-block;
+    padding: 5px;
+    margin: 20px;
+    width: 9rem;
+    border-radius: 3px;
+    border: 2px solid #55917F;
+    background-color: rgba(64, 224, 208, 0.5);
+    font-size: 1.1rem;
+    font-family: Gill Sans;
+`
+
 function SearchBar(props) {
     return (
-        <form>
-            <h3>Search for Recipes</h3>
-            <label>
-                <h5>Search by Title</h5>
-                <input 
+        <SearchForm>
+            <H4>Search for Recipes</H4>
+            <Label>
+                <H5>Search by Title:</H5>
+                <Input 
                     type='text'
                     name='by-title'
                     id='by-title'
                     placeholder='Search by title...'
                 />
-            </label>
-            <label htmlFor='tag-select'>
-                <h3>Search by Category</h3>
-                <select name='tags' id='tag-select'>
+            </Label>
+            <Label htmlFor='tag-select'>
+                <H5>Search by Category:</H5>
+                <Select name='tags' id='tag-select'>
                     <option value=''>Select a tag...</option>
                     {/* map over tags from state to create option elements */}
                     {props.recipes.map((recipeObj, i) => {
@@ -49,10 +110,10 @@ function SearchBar(props) {
                             })
                         )
                     })} */}
-                </select>
-            </label>
-            <button>Search</button>
-        </form>
+                </Select>
+            </Label>
+            <Button>Search</Button>
+        </SearchForm>
     )
 }
 
