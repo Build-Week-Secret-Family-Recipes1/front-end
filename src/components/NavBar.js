adbe0c8eef01a1fbe21ee683bad07c79de04301f
Adam Osburn <wizardofosburn@gmail.com>
2020-06-01 19:51:32 -0400

    corrected merge conflicts

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,32 +1,5 @@
 import React from 'react';
-<<<<<<< HEAD
 import {Link} from 'react-router-dom';
-=======
-import {Link, useParams} from 'react-router-dom';
-import styled from 'styled-components';
-
-const NavBar = styled.div`
-    display: flex;
-    justify-content: flex-end;
-    align-items: center;
-    background-color: rgba(0, 204, 0, 0.7);
-
-`
-
-const Button = styled.button`
-    padding: 10px;
-    margin: 20px;
-    font-size: 1.2rem;
-    border: none;
-    border-radius: 5px;
-    background-color: transparent;
-    color: white;
-
-    &:hover {
-        background-color: #6AD856;
-    }
-`
->>>>>>> master
 
 export default function (){
     const home = {path: '/', name: 'Home'};
@@ -35,7 +8,6 @@ export default function (){
     const nav = [home, recipes, form];
 
     return (
-<<<<<<< HEAD
         <div className='fEKTmX' 
         style={{
             backgroundColor: 'rgba(106,216,86,0.9)',
@@ -56,15 +28,9 @@ export default function (){
                       height: '3.5rem',
                       margin: '1rem'
                   }}>{`${navItem.name}`}</button></Link>
-=======
-        <NavBar className='nav-container'>
-          {nav.map(navItem => (
-              <nav>
-                  <Link to={`${navItem.path}`}><Button>{`${navItem.name}`}</Button></Link>
->>>>>>> master
               </nav>
               
           ))}
-        </NavBar>
+        </div>
     )
 }
